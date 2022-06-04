7b3c422023ed398588f594770236c640e2090456
SergeyGilyazov <s.gilyazov@gmail.com>
2022-06-04 12:57:09 +0500

    more responsive preview

--- src/components/glass.jsx
+++ src/components/glass.jsx
@@ -7,20 +7,31 @@ export default function Glass (props){
     const glass = props.value.glass;
     const rowsCount = glass.length;
     const colsCount = glass[0].length;
-    const addCols = props.preview ? 0:3;
-    const margin = props.preview ? 0:60;
     
-    let cellSize = Math.min((props.value.maxWidth-margin) / (colsCount+addCols), (props.value.maxHight-margin) / rowsCount);
+    const margin = props.preview ? 0:65;
+    
+    
+    let cellSizeByRows = (props.value.maxHight-margin) / rowsCount;
+    let cellSizeByCols =(props.value.maxWidth-margin) / colsCount;
+
+    const direction = cellSizeByRows > cellSizeByCols? 'column':'row';
+    const addCols = props.preview || direction ==='column'? 0:3;
+    const addRows = props.preview || direction ==='row'? 0:4;
+    
+    cellSizeByRows = (props.value.maxHight-margin) / (rowsCount + addRows);
+    cellSizeByCols =(props.value.maxWidth-margin) / (colsCount + addCols);
+    
+    let cellSize = Math.min(cellSizeByRows, cellSizeByCols);
     cellSize = Math.floor(cellSize);
     const rowValue = {cellSize: cellSize}
 
     const rows = glass.map(row=> <Row key={row.id} value={{...rowValue, row:row}}/>)
 
-
+    
     const style={width: `${cellSize *colsCount }px`,
            height: `${cellSize * rowsCount}px`}
     
-    return <div className='glass--outer'>  
+    return <div className='glass--outer' style={{'flex-direction': direction}}>  
                <div className={props.preview?'':'glass--inner'} style={style}>
                      {!props.preview && props.pause && !props.gameOver && <Message message={['PAUSE']}/>}
                      {!props.preview && props.gameOver && 
@@ -29,15 +40,16 @@ export default function Glass (props){
                      {rows}
 
                </div>
-            {!props.preview && <div><Info  width={cellSize*3+10} score={props.value.score} speed={props.value.speed}></Info>
-               <Glass
-          value={{
-            glass: props.previewGlass,
-            maxWidth: cellSize*3,
-            maxHight: cellSize*4,
-          }}
-          preview={true}
-        />
-            </div>}
+            {!props.preview && <div className='glass--preview' style={{'flex-direction': direction ==='row'?'column':'row'}}>
+                                   <Info  width={cellSize*3+10} score={props.value.score} speed={props.value.speed}></Info>
+                                   <Glass
+                                   value={{
+                                   glass: props.previewGlass,
+                                   maxWidth: cellSize*3,
+                                   maxHight: cellSize*4,
+                                   }}
+                                   preview={true}
+                                   />
+                             </div>}
            </div>
 }
